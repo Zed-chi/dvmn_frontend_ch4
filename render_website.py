62e8aabd62b6668eef6d97e7ea4c5667e5e76975
zed <zed@mail.ru>
2020-11-10 15:32:39 +0300

    flake fix

--- render_website.py
+++ render_website.py
@@ -1,40 +1,37 @@
-from jinja2 import Environment, FileSystemLoader, select_autoescape
-from livereload import Server
-from more_itertools import chunked
 import json
 import os
 from urllib.parse import quote
+
+
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+from livereload import Server
+
+from more_itertools import chunked
+
 
 JSON_PATH = os.path.join("./", "books.json")
 HTML_DIR = os.path.join("./", "pages")
 
 
-def get_books_description(path):
-    with open(path, "r", encoding="utf-8") as file:
+def get_books_description_from_json(filepath):
+    with open(filepath, "r", encoding="utf-8") as file:
         data = file.read()
         return json.loads(data)
 
 
 def normalize_data_path(books):
     for book in books:
-        book["img_src"] = quote(book["img_src"].replace("\\", "/").replace(".", "..", 1))
+        book["img_src"] = quote(
+            book["img_src"].replace("\\", "/").replace(".", "..", 1))
         book["book_path"] = quote(
-            book["book_path"].replace("\\", "/").replace(".", "..", 1)
-        )
-
-
-def normalize_data_path_for_local(books):
-    for book in books:
-        book["img_src"] = quote(book["img_src"].replace("\\", "/").replace(".", "", 1))
-        book["book_path"] = quote(
-            book["book_path"].replace("\\", "/").replace(".", "", 1)
-        )
+            book["book_path"].replace("\\", "/").replace(".", "..", 1))
 
 
 def on_reload():
-    description = get_books_description(JSON_PATH)
-    books = description["books"]
-    normalize_data_path(books)
+    description = get_books_description_from_json(JSON_PATH)
+    books_list = description["books"]
+    normalize_data_path(books_list)
     env = Environment(
         loader=FileSystemLoader("."),
         autoescape=select_autoescape(["html", "xml"]),
@@ -45,7 +42,9 @@ def on_reload():
 
     for id, chunk in enumerate(chunks_by_10):
         rendered_page = template.render(
-            books=[*chunked(chunk, 2)], pages_count=pages_count, current_page=id + 1
+            books=[*chunked(chunk, 2)],
+            pages_count=pages_count,
+            current_page=id + 1,
         )
         path = os.path.join(HTML_DIR, f"index{id+1}.html")
         with open(path, "w", encoding="utf8") as file:
